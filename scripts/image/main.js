8732a4cc1d74d9b7ccedbd47e4e4d8d6b0adafab
Nicolas-Asafe <nicolasasafe45@gmail.com>
2025-07-23 12:11:59 -0300

    feat: Select_URL_of_Material

--- scripts/image/main.js
+++
@@ -1,3 +0,0 @@
-export default function Select_img_URL(SwordName){
-    return [`../../assets/images/swords/${SwordName}/use.png`,`../../assets/images/swords/${SwordName}/select.gif`]
-}
---
+++ scripts/image_scripts/main.js
@@ -0,0 +1,6 @@
+export function Select_URL_of_Sword(SwordName){
+    return [`../../assets/images/swords/${SwordName}/use.png`,`../../assets/images/swords/${SwordName}/select.gif`]
+}
+export function Select_URL_of_Material(MaterialName){
+    return [`../../assets/images/materials/${MaterialName}/use.png`]
+}
